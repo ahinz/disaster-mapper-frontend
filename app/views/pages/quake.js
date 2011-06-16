5eb62e87e17bbb1e738131bce478cb8bc6b59342
= <hinz.adam@gmail.com>
2011-06-16 15:50:50 -0400

    update flood and hurricane content. fix earthquakes

--- app/views/pages/quake.js
+++ app/views/pages/quake.js
@@ -1,3 +1,6 @@
 $.subscribe("/update/quake_content", function( content, map_div ) {
-
+    var jq = $("#quake_content");
+    $.each( content, function(key, value) {
+	jq.html(jq.html().replace("_" + key + "_", parseFloat(value)));
+    });
 });
